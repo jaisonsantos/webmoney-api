53e1c63844ebee43cc94e6ba1e61fafb25bc47ae
jaison_js@hotmail.com <brsantos@@001>
2021-12-21 23:41:49 +0100

    Initial commit

---
+++ src/main/java/com/jsquoolt/webmoneyapi/WebmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsquoolt.webmoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsquoolt/webmoneyapi/WebmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsquoolt.webmoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
